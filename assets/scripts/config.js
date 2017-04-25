2a82816a6f270227bd52c22fef3ab283786c5423
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-04-25 17:36:18 -0400

    Switched production URL to appropriate Heroku account

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,8 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://ga-wdi-boston.herokuapp.com'
+    production: 'https://shielded-hamlet-87365.herokuapp.com/'
+    // production: 'https://ga-wdi-boston.herokuapp.com'
   }
 }
 
