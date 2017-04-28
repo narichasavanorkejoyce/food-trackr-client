16106dc9d883aebb258774c426c1c23c8cc44ff0
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-04-28 16:25:09 -0400

    Developed framework for handlebars (food list and grocery list)
    When I click "FOOD", I want to display my existing food item names
    and the number of days left to expiration.
    
    When I click "SHOP", I want to display my grocery list (food name
    and name of the store).
    
    A very ugly list is now displayed on the page. The number of days
    until expiration is not populating yet because I have yet to add that
    variable to the model.

--- assets/scripts/trackr-auth/ui.js
+++ assets/scripts/trackr-auth/ui.js
@@ -1,5 +1,7 @@
 'use strict'
 const store = require('../store.js')
+const displayFoodItemsTemplate = require('../templates/get-food-items.handlebars')
+const displayGroceryListTemplate = require('../templates/get-grocery-list.handlebars')
 
 const addNewItemSuccess = (data) => {
   console.log('addNewItemSuccess worked!')
@@ -14,6 +16,8 @@ const addNewItemFail = (error) => {
 const getFoodItemsSuccess = (data) => {
   console.log(data)
   console.log('getAllItems worked!')
+  const displayFoodItems = displayFoodItemsTemplate({ items: data.items })
+  $('#show-food-list').append(displayFoodItems)
 }
 
 const getFoodItemsFail = (error) => {
@@ -24,6 +28,8 @@ const getFoodItemsFail = (error) => {
 const getGroceryListSuccess = (data) => {
   console.log(data)
   console.log('getAllItems worked!')
+  const displayGroceryList = displayGroceryListTemplate({ items: data.items })
+  $('#show-grocery-list').append(displayGroceryList)
 }
 
 const getGroceryListFail = (error) => {
