25e9a85c536c5999c9d0ee480970a9a86691e208
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-04-30 17:47:44 -0400

    Initial styling using Bootstrap
    Added jumbotrons for intro, sign-up, sign-in, change password,
    sign out, add item.
    
    Added fixed navbar and footer

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,6 +4,7 @@ const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 const authEvents = require('./auth/events.js')
 const trackrEvents = require('./trackr-auth/events.js')
+const uxLogic = require('./uxLogic')
 
 // On document ready
 $(() => {
@@ -18,6 +19,11 @@ $(() => {
 // Bring in tracker events
 $(() => {
   trackrEvents.trackrHandlers()
+})
+
+// Bring in ux logic functions
+$(() => {
+  uxLogic.uxLogicHandlers()
 })
 
 // use require with a reference to bundle the file and use it in this file
---
+++ assets/scripts/uxLogic.js
@@ -0,0 +1,36 @@
+const uxLogicHandlers = () => {
+  // $('#game-info').hide()
+  // $('#play-tic-tac-toe').hide()
+
+//   $('#user-starts-game').on('submit', function () {
+//     $('#play-tic-tac-toe').show()
+//     $('#user-logs-in').hide()
+//     $('#old-password').hide().val('')
+//     $('#new-password').hide().val('')
+//     $('#submit-change-pwd').hide()
+//     $('#num-games-completed').val('')
+//     $('#num-x-wins').val('')
+//     $('#num-o-wins').val('')
+//   })
+//
+//   $('#sign-out').on('submit', function () {
+//     $('#play-tic-tac-toe').hide()
+//     $('#user-logs-in').show()
+//     $('#sign-up-email').val('')
+//     $('#sign-up-password').val('')
+//     $('#sign-up-confirm').val('')
+//     $('#sign-in-email').val('')
+//     $('#sign-in-password').val('')
+//     $('.auth-responses').text('')
+//   })
+//
+//   $('#change-pwd').on('click', function () {
+//     $('#old-password').show()
+//     $('#new-password').show()
+//     $('#submit-change-pwd').show()
+//   })
+}
+
+module.exports = {
+  uxLogicHandlers
+}
